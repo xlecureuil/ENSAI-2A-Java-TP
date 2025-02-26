4c4f16eaca0105249ab1116921f6f6f75306a15c
xlecureuil <xavier.braquaval@eleve.ensai.fr>
2025-02-26 11:14:07 +0000

    exo login

--- tp1/src/Login.java
+++ tp1/src/Login.java
@@ -2,17 +2,43 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Login {
     public static void main(String[] args) {
-        HashMap<String, String> userDatabase = loadUserDatabase("../data/user_hashpwd.csv");
+        HashMap<String, String> userDatabase = loadUserDatabase("/ENSAI-2A-Java-TP/tp1/src/data/user_hashpwd.csv");
         Scanner scanner = new Scanner(System.in);
+        Scanner scanner_2 = new Scanner(System.in);
 
         while (true) {
 
-            // Code here
-        }
+            System.out.print("Enter username: ");
+            String username = scanner.nextLine();
+
+            scanner.close();
+
+            for (Map.Entry<String, String> entry : userDatabase.entrySet()) {
+                String key = entry.getKey();
+                Integer value = entry.getValue();
+                if (userDatabase.containsKey(username)) {
+
+                    System.out.print("Enter password: ");
+                    String password = scanner_2.nextLine();
+
+                    scanner_2.close();
+
+                    if(userDatabase.containsValue(password)){
+                        System.out.println("Login successful!");
+                    } else {
+                        System.out.println("Password Incorrect");
+                    }
+                    
+                } else {
+                    System.out.println("Username does not exist in the database.");
+                
+                }
+            }
     }
 
     /**
