7bfe84fe70b92b867e12824cb75a3ffe06d22379
xlecureuil <xavier.braquaval@eleve.ensai.fr>
2025-03-10 09:26:13 +0000

    library

---
+++ tp2/src/main/java/fr/ensai/library/Library.java
@@ -0,0 +1,56 @@
+package fr.ensai.library;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Library {
+    /**
+     * Loads books from a CSV file and adds them to the library.
+     * 
+     * @param filePath The path to the CSV file containing book data.
+     * @throws IOException If there is an error reading the file, an
+     *                     {@link IOException} will be thrown.
+     */
+    public void loadBooksFromCSV(String filePath) {
+
+        URL url = getClass().getClassLoader().getResource(filePath);
+
+        try (BufferedReader br = new BufferedReader(new FileReader(url.getFile()))) {
+            Map<String, Author> authors = new HashMap<>();
+            String line;
+            br.readLine(); // Skip the header line
+
+            while ((line = br.readLine()) != null) {
+                String[] data = line.split(",");
+
+                if (data.length == 5) {
+                    String isbn = data[0].trim();
+                    String title = data[1].trim();
+                    String authorName = data[2].trim();
+                    int year = Integer.parseInt(data[3].trim());
+                    int pageCount = Integer.parseInt(data[4].trim());
+
+                    // Check if author already exists in the map
+                    Author author = authors.get(authorName);
+                    if (author == null) {
+                        author = new Author(authorName);
+                        authors.put(authorName, author);
+                        System.out.println(author.toString());
+                    }
+                    Book book = new Book(isbn, title, author, year, pageCount);
+
+                    this.addIem(book);
+                }
+            }
+        } catch (
+
+        IOException e) {
+            System.err.println("Error reading the file: " + e.getMessage());
+        }
+    }
+}
+
+
